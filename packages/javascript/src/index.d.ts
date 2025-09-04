701d747f339e8298530ca78176946eb40e6b193f
luke <brainwith2019@gmail.com>
2025-09-04 11:58:44 +0800

    fix tt export

--- packages/javascript/src/index.d.ts
+++ packages/javascript/src/index.d.ts
@@ -10,3 +10,4 @@ export * from "./gen/livekit_rtc_pb.js";
 export * from "./gen/livekit_sip_pb.js";
 export * from "./gen/livekit_webhook_pb.js";
 export * from "./gen/version.js";
+export * from "./gen/livekit_temptalk_pb.js";
