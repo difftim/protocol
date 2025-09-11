6e8bc58e5829e9a371afbbb0c88d2ab3be00ab2c
luke <brainwith2019@gmail.com>
2025-09-11 17:27:37 +0800

    add user agent

--- packages/swift/generate.sh
+++ packages/swift/generate.sh
@@ -7,4 +7,11 @@ PROTO_DIR="$SCRIPT_DIR/../../protobufs"
 OUT_DIR="$SCRIPT_DIR/dist"
 
 mkdir -p "$OUT_DIR"
-protoc --swift_out="$OUT_DIR" -I="$PROTO_DIR" "$PROTO_DIR"/livekit_*.proto
+
+protoc --swift_out="$OUT_DIR" --swift_opt=Visibility=Public -I="$PROTO_DIR" "$PROTO_DIR/livekit_temptalk.proto"
+
+for file in "$PROTO_DIR"/livekit_*.proto; do
+  if [[ "$file" != "$PROTO_DIR/livekit_temptalk.proto" ]]; then
+    protoc --swift_out="$OUT_DIR" -I="$PROTO_DIR" "$file"
+  fi
+done
